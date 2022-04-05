cd156560177a9504e79b4a0ae308f32ea3ea2cee
RodrigoLima96 <rodrigolima9657@gmail.com>
2022-04-05 11:16:38 -0300

    add search page

---
+++ lib/pages/search/search_page.dart
@@ -0,0 +1,98 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:instagram_clone/utils/colors.dart';
+
+class SearchPage extends StatefulWidget {
+  const SearchPage({Key? key}) : super(key: key);
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  final TextEditingController searchController = TextEditingController();
+  bool showUsers = false;
+  @override
+  void dispose() {
+    super.dispose();
+    searchController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: mobileBackgroundColor,
+        title: TextFormField(
+          controller: searchController,
+          decoration: const InputDecoration(
+            labelText: 'Search for a user',
+            border: InputBorder.none,
+          ),
+          onFieldSubmitted: (String _) {
+            if (searchController.text.isNotEmpty) {
+              setState(() {
+                showUsers = true;
+              });
+            }
+          },
+        ),
+      ),
+      body: showUsers
+          ? FutureBuilder(
+              future: FirebaseFirestore.instance
+                  .collection('users')
+                  .where(
+                    'username',
+                    isGreaterThanOrEqualTo: searchController.text,
+                  )
+                  .get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+
+                return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          snapshot.data!.docs[index]['photoUrl'],
+                        ),
+                        radius: 16,
+                      ),
+                      title: Text(snapshot.data!.docs[index]['username']),
+                    );
+                  },
+                );
+              },
+            )
+          : FutureBuilder(
+              future: FirebaseFirestore.instance.collection('posts').get(),
+              builder: (context,
+                  AsyncSnapshot<QuerySnapshot<Map<String, dynamic>>> snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+                return StaggeredGridView.countBuilder(
+                  crossAxisCount: 3,
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) =>
+                      Image.network(snapshot.data!.docs[index]['postUrl']),
+                  staggeredTileBuilder: (index) => StaggeredTile.count(
+                    (index % 7 == 0) ? 2 : 1,
+                    (index % 7 == 0) ? 2 : 1,
+                  ),
+                  mainAxisSpacing: 8,
+                  crossAxisSpacing: 8,
+                );
+              },
+            ),
+    );
+  }
+}
--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/pages/addPost/add_post_page.dart';
 import 'package:instagram_clone/pages/feed/feed_page.dart';
+import 'package:instagram_clone/pages/search/search_page.dart';
 
 const webPageSize = 600;
 
 List<Widget> homePageItems = [
   const FeedPage(),
-  const Text('search'),
+  const SearchPage(),
   const AddPostPage(),
   const Text('like'),
   const Text('profile'),
